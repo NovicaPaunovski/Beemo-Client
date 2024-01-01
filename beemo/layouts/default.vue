7462d18ee2af73616b4350c4793fbaad8d3b56f0
Novica Paunovski <124404839+NovicaPaunovski@users.noreply.github.com>
2024-01-01 20:54:09 +0100

    Edited default layout

--- beemo/layouts/default.vue
+++ beemo/layouts/default.vue
@@ -2,20 +2,20 @@
     <div>
       <header class="shadow-sm bg-white">
           <nav class="container mx-auto p-4 flex justify-between items-center">
-              <NuxtLink to="/" class="font-bold flex gap-2 items-center"><img class="w-48" src="../assets/content/logo.svg"/></NuxtLink>
+              <NuxtLink to="/" class="font-bold flex gap-2 items-center mr-5"><img class="w-48" src="../assets/content/logo.svg"/></NuxtLink>
               <ul class="flex gap-4 items-center">
                   <li><NuxtLink to="/"><p>Home</p></NuxtLink></li>
-                  <li><NuxtLink to="/about"><p>About</p></NuxtLink></li>
                   <li><NuxtLink to="/budgets"><p>Budgets</p></NuxtLink></li>
                   <li class="border-l">
                     <template v-if="!isAuthenticated">
-                      <button class="btn ml-4" @click="() => open()">Login</button>
-                      <NuxtLink to="/sign-up">
-                        <button class="btn-secondary ml-2">Sign Up</button>
-                      </NuxtLink>
+                      <div class="flex">
+                        <button class="btn ml-4" @click="() => open()">Login</button>
+                        <NuxtLink to="/sign-up" class="ml-2">
+                          <button class="btn-secondary">Sign Up</button>
+                        </NuxtLink>
+                      </div>
                     </template>
                     <template v-else>
-                    <!-- Render user profile thumbnail here -->
                     <img src="../assets/content/logo.svg" alt="User Profile" class="w-8 h-8 rounded-full ml-4">
                     </template>
                   </li>
