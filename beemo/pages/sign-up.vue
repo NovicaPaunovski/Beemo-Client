1de652b7e1fb407f6c15332126ddbac57a9cc844
Novica Paunovski <124404839+NovicaPaunovski@users.noreply.github.com>
2024-02-09 23:31:39 +0100

    Updated sign up form rules

--- beemo/pages/sign-up.vue
+++ beemo/pages/sign-up.vue
@@ -2,11 +2,35 @@
     <div class="mt-7 max-w-sm mx-auto text-center card">
       <h1 class="text-xl mt-4">Sign up to Beemo</h1>
       <form @submit.prevent="handleSignup" class="mt-7 mx-4 px-4 rounded border-t">
+        <label v-show="invalid.name" for="name" class="mt-4 block text-left text-red-500">Please enter your full name.</label>
+        <div id="name" class="flex w-full">
+          <div>
+            <label for="first-name" class="block text-left text-gray-600">First name:</label>
+            <input
+              @blur="() => validNameRule(form.firstName)"
+              type="text"
+              id="first-name"
+              v-model="form.firstName"
+              class="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500"
+            />
+          </div>
+          <div class="ml-2">
+            <label for="last-name" class="block text-left text-gray-600">Last name:</label>
+            <input
+            @blur="() => validNameRule(form.lastName)"
+              type="text"
+              id="last-name"
+              v-model="form.lastName"
+              class="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500"
+            />
+          </div>
+        </div>
+
         <div class="mt-4">
-          <label v-show="invalid.username" class="block text-left text-red-500">Your username needs to be at least 8 letters long.</label>
+          <label v-show="invalid.username" class="block text-left text-red-500">Please enter a valid username at least 8 letters long.</label>
           <label for="username" class="block text-left text-gray-600">Username:</label>
           <input
-            @blur="validateUsername"
+            @blur="validUsernameRule"
             type="text"
             id="username"
             v-model="form.username"
@@ -18,7 +42,7 @@
           <label v-show="invalid.email" class="block text-left text-red-500">Please enter a valid email address.</label>
           <label for="email" class="block text-left text-gray-600">Email:</label>
           <input
-            @blur="validateEmail"
+            @blur="validEmailRule"
             type="text"
             id="email"
             v-model="form.email"
@@ -30,7 +54,7 @@
           <label v-if="invalid.password" class="block text-left text-red-500">Your password is not strong enough.</label>
           <label for="password" class="block text-left text-gray-600">Password:</label>
           <input
-            @blur="validatePassword"
+            @blur="validPasswordRule"
             type="password"
             id="password"
             v-model="form.password"
@@ -42,7 +66,7 @@
           <label v-if="invalid.matchPassword" class="block text-left text-red-500">The passwords do not match.</label>
           <label for="confirm-password" class="block text-left text-gray-600">Confirm Password:</label>
           <input
-            @blur="validateMatchingPassword"
+            @blur="matchingPasswordRule"
             type="password"
             id="confirm-password"
             v-model="form.confirmPassword"
@@ -51,7 +75,7 @@
         </div>  
 
         <div class="mt-4 flex justify-start">
-            <input id="confirm-tos-checkbox" v-model="confirmedTos" type="checkbox" class="mt-2 w-4 h-4 checkbox"/>
+            <input id="confirm-tos-checkbox" v-model="invalid.unconfirmedTos" :true-value="false" :false-value="true" type="checkbox" class="mt-2 w-4 h-4 checkbox"/>
             <div class="ml-2 flex justify-start items-start text-left">
                 <label class="text-gray-600">I have read and agree to the <span class="cursor-pointer text-blue-500 text-base mt-4">Beemo Terms and Conditions</span></label>
             </div>
@@ -79,6 +103,8 @@ const { loading,  failed } = storeToRefs(useAuthStore());
 const success = ref(false);
 
 const form = ref({
+  firstName: '',
+  lastName: '',
   username: '',
   email: '',
   password: '',
@@ -86,22 +112,35 @@ const form = ref({
 })
 
 const invalid = ref({
+    name: false,
     username: false,
     password: false,
     matchPassword: false,
     email: false,
+    unconfirmedTos: true
 })
 
-const confirmedTos = ref(false);
+const checkInvalidFormValues = () => {
+  return (Object.values(invalid.value).some(value => value == true)) || Object.values(form.value).some(value => value == '');
+}
+
+const focusEmptyFormValues = () => {
+  const formInputs = document.querySelectorAll('input');
+  formInputs.forEach((formInput) => {
+    if (formInput.value == '' || formInput.value == 'on')
+    {
+        formInput.classList.add("border-blue-500");
+    }
+    else {
+      formInput.classList.remove("border-blue-500");
+    }
+  })
+}
 
 const handleSignup = async () => {
-  if (confirmedTos.value || (Object.values(invalid.value).some(value => value == true)) || Object.values(form.value).some(value => value == ''))
+  if (checkInvalidFormValues())
   {
-    if (!confirmedTos.value) {
-      //TODO: Fix autofocus
-      document.getElementById("confirm-tos-checkbox")?.focus();
-      console.log(document.getElementById("confirm-tos-checkbox"));
-    }
+    focusEmptyFormValues();
     return;
   }
 
@@ -114,47 +153,26 @@ const handleSignup = async () => {
   success.value = true;
 }
 
-const validateUsername = () => {
-  if (form.value.username.length < 8) {
-    invalid.value.username = true;
-  }
-  else {
-    invalid.value.username = false;
-  }
+const validNameRule = (value : string) => {
+  invalid.value.name = !/^[a-zA-Z]+$/.test(value);
 }
 
-const validatePassword = () => {
-  if (form.value.password.length < 8) {
-    invalid.value.password = true;
-  }
-  else {
-    invalid.value.password = false;
-  }
+const validUsernameRule = () => {
+  invalid.value.username =  form.value.username != '' && !/^[0-9A-Za-z._-]{8,32}$/.test(form.value.username);
 }
 
-const validateMatchingPassword = () => {
-  if (form.value.password != form.value.confirmPassword) {
-    invalid.value.matchPassword = true;
-  }
-  else {
-    invalid.value.matchPassword = false;
-  }
+const validPasswordRule = () => {
+  invalid.value.password =  form.value.password.length < 8 && form.value.password != '';
 }
 
-const validateEmail = () => {
+const matchingPasswordRule = () => {
+  invalid.value.matchPassword = form.value.password != form.value.confirmPassword;
+}
+
+const validEmailRule = () => {
   const mailformat =
         /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
-  if (!form.value.email.match(mailformat) && form.value.email != '')
-  {
-    invalid.value.email = true;
-  }
-  else {
-    invalid.value.email = false;
-  }
+  invalid.value.email = !form.value.email.match(mailformat) && form.value.email != '';
 }
 </script>
-
-<style scoped>
-
-</style>
